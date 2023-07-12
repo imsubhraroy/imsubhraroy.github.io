398ea2578e9700f5018f5301b97f2cfb5e4cf142
Subhra Roy <subhraroy894@gmail.com>
2023-07-12 17:32:48 +0530

    1.0.2

--- Admin/layout/isadmin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// session_start();
-//To check  if user is login or nnot
-if(!isset($_SESSION['is_admin'])){
-    header('location: ../login.php');
-}
-
-?>
--- Technician/istechnician.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//To check  if user is login or nnot
-if(!isset($_SESSION['is_technician'])){
-    header('location: technicianlogin.php');
-}
-
-?>
